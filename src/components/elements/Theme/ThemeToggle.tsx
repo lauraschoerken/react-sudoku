bd47338cd14396f581167e0ecc81edf579caa70f
David Hormiga Afonso <davidhormigafonso@gmail.com>
2025-09-15 22:01:01 +0200

    refactor: replace inline SVG icons with imported SVG files and remove unused icons

--- src/components/elements/Theme/ThemeToggle.tsx
+++ src/components/elements/Theme/ThemeToggle.tsx
@@ -2,9 +2,9 @@ import './ThemeToggle.scss'
 
 import { useContext } from 'react'
 
+import MoonIcon from '@/assets/svgs/moon.svg?react'
+import SunIcon from '@/assets/svgs/sun.svg?react'
 import { ThemeContext } from '@/utils/Theme/theme-context'
-
-import { MoonIcon,SunIcon } from './icons'
 
 export const ThemeToggle = () => {
 	const { theme, toggle } = useContext(ThemeContext)
