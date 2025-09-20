e3277a49deac0936160e96e1fa7ce402e17a23a9
Laura Schörken <lauraschorken@gmail.com>
2025-09-20 21:10:35 +0200

    fix: update import path for AppDispatch and RootState in hooks

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,10 +1,8 @@
 import type { TypedUseSelectorHook } from 'react-redux'
 import { useDispatch, useSelector } from 'react-redux'
 
-import type { AppDispatch, RootState } from './index'
+import type { AppDispatch, RootState } from './features/settings/index'
 
-// ✅ useDispatch tipado
 export const useAppDispatch: () => AppDispatch = useDispatch
 
-// ✅ useSelector tipado
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
