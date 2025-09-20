baa82042f5172b346c5baa3603fd93d9333c8704
Laura Schörken <lauraschorken@gmail.com>
2025-09-20 21:11:29 +0200

    feat: use settings hooks

--- src/components/Settings/components/SettingsComponent.tsx
+++ src/components/Settings/components/SettingsComponent.tsx
@@ -1,24 +1,22 @@
 import './SettingsComponent.scss'
 
 import { useTranslation } from 'react-i18next'
-import { useDispatch, useSelector } from 'react-redux'
 
 import LanguageSelect from '@/components/elements/Languague/LanguagueSelect'
 import { ThemeToggle } from '@/components/elements/Theme/ThemeToggle'
-import type { RootState } from '@/store'
-import {
-	setCronometroEnabled,
-	setCronometroTipo,
-	setLimitadorErroresEnabled,
-	setLimiteErrores,
-	toggleErroresActivos,
-} from '@/store/settingsSlice'
+import { useErrorLimit } from '@/hooks/settings/useErrorLimit'
+import { useErrors } from '@/hooks/settings/useErrors'
+import { useTimer } from '@/hooks/settings/useTimer'
 
 export default function SettingsComponent() {
-	const { t } = useTranslation('settings') // 👈 namespace
-	const dispatch = useDispatch()
-	const { erroresActivos, cronometro, cronometroTipo, limitadorErrores, limiteErrores } =
-		useSelector((s: RootState) => s.settings)
+	const { t } = useTranslation('settings')
+
+	const { errorsActive, toggleErrorsActive } = useErrors()
+
+	const { errorsLimiterEnabled, errorsLimit, setErrorsLimiterEnabled, setErrorsLimit } =
+		useErrorLimit()
+
+	const { timerEnabled, timerMode, setTimerEnabled, setTimerMode } = useTimer()
 
 	return (
 		<div className='page-wrapper'>
@@ -32,29 +30,29 @@ export default function SettingsComponent() {
 					<label className='inline'>
 						<input
 							type='checkbox'
-							checked={erroresActivos}
-							onChange={() => dispatch(toggleErroresActivos())}
+							checked={errorsActive}
+							onChange={toggleErrorsActive}
 							aria-label={t('errors.active')}
 						/>
 						{t('errors.active')}
 					</label>
 
-					{erroresActivos && (
+					{errorsActive && (
 						<div className='error-limit'>
 							<label className='inline'>
 								<input
 									type='checkbox'
-									checked={limitadorErrores}
-									onChange={() => dispatch(setLimitadorErroresEnabled(!limitadorErrores))}
+									checked={errorsLimiterEnabled}
+									onChange={() => setErrorsLimiterEnabled(!errorsLimiterEnabled)}
 									aria-label={t('errors.limit.toggle')}
 								/>
 								{t('errors.limit.toggle')}
 							</label>
 
 							<select
-								value={limiteErrores}
-								disabled={!limitadorErrores}
-								onChange={(e) => dispatch(setLimiteErrores(Number(e.target.value) as 3 | 5 | 10))}
+								value={errorsLimit}
+								disabled={!errorsLimiterEnabled}
+								onChange={(e) => setErrorsLimit(Number(e.target.value) as 3 | 5 | 10)}
 								aria-label={t('errors.limit.select')}>
 								<option value='3'>3</option>
 								<option value='5'>5</option>
@@ -69,19 +67,17 @@ export default function SettingsComponent() {
 					<label>
 						<input
 							type='checkbox'
-							checked={cronometro}
-							onChange={() => dispatch(setCronometroEnabled(!cronometro))}
+							checked={timerEnabled}
+							onChange={() => setTimerEnabled(!timerEnabled)}
 							aria-label={t('timer.title')}
 						/>
 						{t('timer.title')}
 					</label>
 
-					{cronometro && (
+					{timerEnabled && (
 						<select
-							value={cronometroTipo}
-							onChange={(e) =>
-								dispatch(setCronometroTipo(e.target.value as 'countdown' | 'normal'))
-							}
+							value={timerMode}
+							onChange={(e) => setTimerMode(e.target.value as 'countdown' | 'normal')}
 							aria-label={t('timer.mode')}>
 							<option value='countdown'>{t('timer.countdown')}</option>
 							<option value='normal'>{t('timer.normal')}</option>
